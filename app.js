14bdcd3ea865feb2d9c996a0981a6dd992984391
Manohar Kumar <manohar.csengineer@gmail.com>
2024-10-18 23:04:57 +0530

    commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 
-// Socket io runs on http server so we hav create http server
+// Socket io runs on http server
 const http = require("http");
 
 // Socket.io Setup
 const socketio = require("socket.io");
+// Creating http server
 const server = http.createServer(app);
 const io = socketio(server);
 
